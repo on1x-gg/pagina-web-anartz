ae5d51f87d60165e002eaf7bada74fa5067ee55a
on1x <anartz.cabanelas@gmail.com>
2025-10-31 14:07:43 +0100

    primer commit

---
+++ src/HolaMundoo/Holamundoo.java
@@ -0,0 +1,12 @@
+package HolaMundoo;
+import java.util.Scanner;
+public class Holamundoo {
+
+	public static void main(String[] args) {
+		Scanner teclado=new Scanner(System.in);
+		System.out.println("Introduce una letra");
+		String letra=teclado.nextLine();
+		teclado.close();
+	}
+
+}
